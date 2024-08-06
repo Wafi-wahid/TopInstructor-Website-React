79ac4d48f20046045d32e71e95582419b02d5702
Wafi-wahid <wafi.wahid26@gmail.com>
2024-08-06 21:57:45 +0500

    modified

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const fname = "wafi";
+const lname = "wahid";
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // React 18
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <header>Hello World</header>
+    <p>It's my first React project</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>{fname + " " + lname}</li>
+    </ul>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
