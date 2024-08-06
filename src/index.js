b3038ae48e25f6ef35c071364b87901adbb1cc45
Wafi-wahid <wafi.wahid26@gmail.com>
2024-08-07 00:31:24 +0500

    first challenge completd

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import App from "./Componenets/App";
 
-const fname = "wafi";
-const lname = "wahid";
-
-const root = ReactDOM.createRoot(document.getElementById("root")); // React 18
-root.render(
-  <div>
-    <header>Hello World</header>
-    <p>It's my first React project</p>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-      <li>{fname + " " + lname}</li>
-    </ul>
-  </div>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
